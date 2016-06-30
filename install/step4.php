7054e170aeb4c504563ff9ce8101a6cb0ed67f34
Luqman Yasin <luqman.yasin@gmail.com>
2016-06-30 20:41:52 +0100

    Redirect to admin panel on completion

--- install/step4.php
+++ install/step4.php
@@ -1,2 +1,3 @@
 <p>Congratulations! You've installed Motivote.</p>
 <p>You can now access the admin control panel. <a href="<?php echo(mv_base_url().'/admin/index.php'); ?>">Click here to do so now!</a>
+<meta http-equiv="refresh" content="0;URL=<?php echo(mv_base_url().'/admin/index.php'); ?>">
